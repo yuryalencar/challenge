9ede72051ec02aa728bb91c1b10cae3c63ee60d3
Yury Alencar <yuryalencar19@gmail.com>
2020-06-21 02:57:50 -0300

    tests(models): Test of the Recipe creation

---
+++ tests/models/Recipe.test.js
@@ -0,0 +1,23 @@
+const assert = require('assert');
+const Recipe = require('../../src/models/Recipe');
+
+describe.only('Test Suite of the Recipe Model Creation', function () {
+    it('Check Recipe Model Creation', async () => {
+        let title = 'example';
+        let ingredients = [];
+        let link = 'https://github.com/yuryalencar/challenge';
+        let gif = 'https://github.com/yuryalencar/challenge/gif';
+
+        let expectedResult = {
+            title,
+            ingredients,
+            link,
+            gif,
+        };
+
+        let result = new Recipe(title, ingredients, link, gif);
+        assert.deepEqual(result, expectedResult);
+    });
+
+    // verificar os tipos?
+});
