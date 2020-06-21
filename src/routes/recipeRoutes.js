4342e9874fc50de666f8f793a3f51fce0fa50278
Yury Alencar <yuryalencar19@gmail.com>
2020-06-21 11:06:38 -0300

    feat(query-validation): Validate amount of the ingredients

--- src/routes/recipeRoutes.js
+++ src/routes/recipeRoutes.js
@@ -1,6 +1,9 @@
 const BaseRoute = require('./base/baseRoute');
+const Joi = require('@hapi/joi');
 
 class RecipeRoutes extends BaseRoute {
+    CHECK_AMOUNT_INGREDIENTS_REGEX = /^[^,]+(,[^,]+)?(,[^,]+)?$/;
+
     constructor() {
         super();
     }
@@ -11,6 +14,15 @@ class RecipeRoutes extends BaseRoute {
             method: 'GET',
             handler: (request, headers) => {
                 return `Hello ${request.query.i}!`;
+            },
+            options: {
+                validate: {
+                    query: Joi.object({
+                        i: Joi.string()
+                            .regex(this.CHECK_AMOUNT_INGREDIENTS_REGEX)
+                            .required(),
+                    }),
+                },
             },
         };
     }
--- tests/routes/recipes.test.js
+++ tests/routes/recipes.test.js
@@ -8,14 +8,54 @@ describe.only('Test Suite of the Recipes API', function () {
         app = await api;
     });
 
-    it('Check List Recipes Status Code is 200 OK', async () => {
+    it('Check List Recipes Status Code is 200 OK (1 ingredient)', async () => {
+        const result = await app.inject({
+            method: 'GET',
+            url: '/recipes/?i=ingredient',
+        });
+
+        const statusCode = result.statusCode;
+        assert.deepEqual(statusCode, 200);
+    });
+
+    it('Check List Recipes Status Code is 200 OK (3 ingredients)', async () => {
+        const result = await app.inject({
+            method: 'GET',
+            url: '/recipes/?i=ingredient,ingredient,ingredient',
+        });
+
+        const statusCode = result.statusCode;
+        assert.deepEqual(statusCode, 200);
+    });
+
+    it('Check List Recipes Status Code is 400 (0 ingredients)', async () => {
+        const result = await app.inject({
+            method: 'GET',
+            url: '/recipes/?i=',
+        });
+
+        const statusCode = result.statusCode;
+        assert.deepEqual(statusCode, 400);
+    });
+
+    it('Check List Recipes Status Code is 400 (4 ingredients)', async () => {
+        const result = await app.inject({
+            method: 'GET',
+            url: '/recipes/?i=ingredient,ingredient,ingredient,ingredient',
+        });
+
+        const statusCode = result.statusCode;
+        assert.deepEqual(statusCode, 400);
+    });
+
+    it('Check List Recipes Status Code is 400 without query param', async () => {
         const result = await app.inject({
             method: 'GET',
             url: '/recipes/',
         });
 
         const statusCode = result.statusCode;
-        assert.deepEqual(statusCode, 200);
+        assert.deepEqual(statusCode, 400);
     });
 
     // test return is a array of recipes
